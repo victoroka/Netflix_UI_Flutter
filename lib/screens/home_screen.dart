416d1b1645ce4710420a9607a72cde8784991619
Victor Hideo Oka <oka.victor97@gmail.com>
2020-04-26 20:24:52 -0300

    add app bar layout

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,6 +8,32 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        title: Image(
+          image: AssetImage('assets/images/netflix_logo.png'),
+          width: 100,
+        ),
+        leading: IconButton(
+          padding: EdgeInsets.only(left: 30.0),
+          onPressed: () => print('menu'),
+          icon: Icon(Icons.menu),
+          iconSize: 30.0,
+          color: Colors.black,
+        ),
+        actions: <Widget>[
+          IconButton(
+            padding: EdgeInsets.only(right: 30.0),
+            onPressed: () => print('search'),
+            icon: Icon(Icons.search),
+            iconSize: 30.0,
+            color: Colors.black,
+          ),
+        ],
+      ),
+    );
   }
 }
