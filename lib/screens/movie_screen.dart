b52547bea3d8b1d9c1a96f455aa7676afa832c9a
Victor Hideo Oka <oka.victor97@gmail.com>
2020-04-29 15:17:41 -0300

    add movie screen layout

--- lib/screens/movie_screen.dart
+++ lib/screens/movie_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:netflix_ui/models/movie_model.dart';
+import 'package:netflix_ui/widgets/circular_clipper.dart';
 
 class MovieScreen extends StatefulWidget {
   final Movie movie;
@@ -13,6 +14,60 @@ class MovieScreen extends StatefulWidget {
 class _MovieScreenState extends State<MovieScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: ListView(
+        children: <Widget>[
+          Stack(
+            children: <Widget>[
+              Container(
+                transform: Matrix4.translationValues(0.0, -50.0, 0.0),
+                child: Hero(
+                  tag: widget.movie.imageUrl,
+                  child: ClipShadowPath(
+                    shadow: Shadow(blurRadius: 20.0),
+                    clipper: CircularClipper(),
+                    child: Image(
+                      height: 400.0,
+                      width: double.infinity,
+                      fit: BoxFit.cover,
+                      image: AssetImage(widget.movie.imageUrl),
+                    ),
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  IconButton(
+                    padding: EdgeInsets.only(left: 30.0),
+                    icon: Icon(Icons.arrow_back),
+                    iconSize: 30.0,
+                    color: Colors.black,
+                    onPressed: () => Navigator.pop(context),
+                  ),
+                  Image(
+                    image: AssetImage('assets/images/netflix_logo.png'),
+                    height: 60.0,
+                    width: 150.0,
+                  ),
+                  IconButton(
+                    padding: EdgeInsets.only(right: 30.0),
+                    icon: Icon(Icons.favorite_border),
+                    iconSize: 30.0,
+                    color: Colors.black,
+                    onPressed: () => print('Add to favourites'),
+                  ),
+                ],
+              ),
+              Positioned.fill(
+                bottom: 10.0,
+                child: Align(),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
